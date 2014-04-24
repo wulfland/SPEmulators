0fce07c554ab6717cddf2b17d66bc8cef3ad8fd2
M. Kaufmann <wulfland@hotmail.com>
2014-04-24 21:57:33 +0200

    More types...

--- SPEmulators/SPEmulators/EmulatedTypes/SimSPFieldLinkCollection.cs
+++ SPEmulators/SPEmulators/EmulatedTypes/SimSPFieldLinkCollection.cs
@@ -1,14 +1,47 @@
 ﻿namespace SPEmulators.EmulatedTypes
 {
     using System;
-    using System.Collections.Generic;
     using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
+    using Microsoft.QualityTools.Testing.Fakes;
+    using Microsoft.QualityTools.Testing.Fakes.Shims;
     using Microsoft.SharePoint;
     using Microsoft.SharePoint.Fakes;
 
     internal class SimSPFieldLinkCollection : CollectionIsolator<SPFieldLink, SPFieldLinkCollection, ShimSPFieldLinkCollection>
     {
+        public SimSPFieldLinkCollection()
+            : this(ShimRuntime.CreateUninitializedInstance<SPFieldLinkCollection>())
+        {
+        }
+
+        public SimSPFieldLinkCollection(SPFieldLinkCollection instance)
+            : base(instance)
+        {
+            base.Fake.Bind(this);
+            base.Fake.AddSPFieldLink = new FakesDelegates.Action<SPFieldLink>(base.Add);
+            base.Fake.ItemGetString = (string name) =>
+            {
+                SPFieldLink fieldLink = this.FirstOrDefault((SPFieldLink f) => f.Name == name);
+                if (fieldLink != null)
+                {
+                    return fieldLink;
+                }
+                throw new ArgumentException();
+            };
+            base.Fake.ItemGetGuid = (Guid id) =>
+            {
+                SPFieldLink fieldLink = this.FirstOrDefault((SPFieldLink f) => f.Id == id);
+                if (fieldLink != null)
+                {
+                    return fieldLink;
+                }
+                throw new ArgumentException();
+            };
+        }
+
+        internal static void Initialize()
+        {
+            //ShimSPFieldLinkCollection.set_Behavior(EmulatorUtilities.NotSupportedBehavior);
+        }
     }
 }
