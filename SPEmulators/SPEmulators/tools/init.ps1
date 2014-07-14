36a6e2e5a44e615ae5b45841416a2d12b7e54ec4
wulfland <wulfland@hotmail.com>
2014-07-13 23:45:59 -0700

    Allow visual studio 2013 premuim since it supports ms fakes.

--- SPEmulators/SPEmulators/tools/init.ps1
+++ SPEmulators/SPEmulators/tools/init.ps1
@@ -1,4 +1,15 @@
-if (($DTE.Edition -ne "Ultimate") -or ($DTE.Version -lt "11"))
+if ($DTE.Version -lt "11")
 {
-	throw "This package requires minimum Visual Studio 2012 Ultimate"
+	throw "This package requires minimum Visual Studio 2012"
+}
+if (($DTE.Version -eq "11.0") -and ($DTE.Edition -ne "Ultimate"))
+{
+	throw "This package requires the Ultimate Edition of Visual Studio 2012"
+}
+if ($DTE.Version -eq "12.0")
+{
+	if (($DTE.Edition -ne "Premium") -and ($DTE.Edition -ne "Ultimate"))
+	{
+		throw "This package requires minimum the Premium Edition of Visual Studio 2013"
+	}
 }
