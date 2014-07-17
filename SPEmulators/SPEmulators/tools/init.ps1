c496594a38f5b14712024ccc3d68f5134b92f9ce
wulfland <wulfland@hotmail.com>
2014-07-17 04:35:43 -0700

    Allow premium edition for visual studio update 2 or greater.

--- SPEmulators/SPEmulators/tools/init.ps1
+++ SPEmulators/SPEmulators/tools/init.ps1
@@ -1,15 +1,16 @@
-if ($DTE.Version -lt "11")
-{
-	throw "This package requires minimum Visual Studio 2012"
+# Fakes was introduced in VS2012
+if ($DTE.Version -lt "11"){
+	throw "This package requires minimum Visual Studio 2012."
 }
-if (($DTE.Version -eq "11.0") -and ($DTE.Edition -ne "Ultimate"))
-{
-	throw "This package requires the Ultimate Edition of Visual Studio 2012"
-}
-if ($DTE.Version -eq "12.0")
-{
-	if (($DTE.Edition -ne "Premium") -and ($DTE.Edition -ne "Ultimate"))
-	{
-		throw "This package requires minimum the Premium Edition of Visual Studio 2013"
+
+$BuildNumber = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($DTE.FullName).ProductBuildPart
+if ($BuildNumber -ge "60315"){
+	# Since VS2012 it is available in the PremiumEdition
+	if (-not ($DTE.Edition -eq "Premium") -or ($DTE.Edition -eq "Ultimate")){
+		throw "This package requires minimum the Premium Edition of Visual Studio 2012 Update 2."
+	}
+}else{
+	if ($DTE.Edition -ne "Ultimate"){
+		throw "This package requires minimum the Ultimate Edition of Visual Studio 2012 if you have not updated to Update 2."
 	}
 }
