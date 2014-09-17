ed521f85d24957d39644182cf2c1ba33102a300c
wulfland <wulfland@hotmail.com>
2014-09-17 09:22:23 -0700

    added fix for init.ps1

--- SPEmulators/SPEmulators/tools/init.ps1
+++ SPEmulators/SPEmulators/tools/init.ps1
@@ -4,9 +4,9 @@ if ($DTE.Version -lt "11"){
 }
 
 $BuildNumber = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($DTE.FullName).ProductBuildPart
-if ($BuildNumber -ge "60315"){
+if ($BuildNumber -ge “60315” -or $DTE.Version -ge “12”){
 	# Since VS2012 it is available in the PremiumEdition
-	if (-not ($DTE.Edition -eq "Premium") -or ($DTE.Edition -eq "Ultimate")){
+	if (-not (($DTE.Edition -eq “Premium”) -or ($DTE.Edition -eq “Ultimate”))){
 		throw "This package requires minimum the Premium Edition of Visual Studio 2012 Update 2."
 	}
 }else{
